6b7db582219437cea1959f153a37c2c408fad7c1
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-25 23:57:05 +0500

    Question05

---
+++ Question05/Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_Person = "Quaid-e-Azam Muhammad Ali Jinnah";
+let quote = "'A nation can never make progress unless it marches in one formation.'";
+let message = `${famous_Person} once said, ${quote}`;
+console.log(message);
---
+++ Question05/Q5.ts
@@ -0,0 +1,4 @@
+let famous_Person: string = "Quaid-e-Azam Muhammad Ali Jinnah";
+let quote: string = "'A nation can never make progress unless it marches in one formation.'";
+let message: string = `${famous_Person} once said, ${quote}`;
+console.log(message);
