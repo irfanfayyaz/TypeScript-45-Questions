dea21b65b6d62784d82d37975f99217b495d2d6c
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-25 23:45:55 +0500

    Question04

---
+++ Question04/Q4.js
@@ -0,0 +1,4 @@
+"use strict";
+let author = "Quaid-e-Azam Muhammad Ali Jinnah";
+let quote = "'A nation can never make progress unless it marches in one formation.'";
+console.log(`${author} once said, ${quote}`);
---
+++ Question04/Q4.ts
@@ -0,0 +1,3 @@
+let author: string = "Quaid-e-Azam Muhammad Ali Jinnah";
+let quote: string = "'A nation can never make progress unless it marches in one formation.'";
+console.log(`${author} once said, ${quote}`);
