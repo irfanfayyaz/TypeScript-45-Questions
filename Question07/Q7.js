cc87a1d07a4dde8c98060f29b197a655fd8a4ead
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-26 09:32:38 +0500

    Question07 solved and added

---
+++ Question07/Q7.js
@@ -0,0 +1,9 @@
+"use strict";
+// Addition Operation
+console.log("Addition:", 5 + 3);
+// Subtraction Operation
+console.log("Subtraction:", 12 - 4);
+// Multiplication Operation
+console.log("Multiplication:", 4 * 2);
+// Division operation
+console.log("Division:", 24 / 3);
---
+++ Question07/Q7.ts
@@ -0,0 +1,11 @@
+// Addition Operation
+console.log("Addition:", 5 + 3);
+
+// Subtraction Operation
+console.log("Subtraction:", 12 - 4);
+
+// Multiplication Operation
+console.log("Multiplication:", 4 * 2);
+
+// Division operation
+console.log("Division:", 24 / 3);
