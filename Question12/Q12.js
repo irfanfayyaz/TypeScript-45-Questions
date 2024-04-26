525a9c772e9452d00e9d2598fcbae267f6e50179
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-26 15:08:10 +0500

    Question12 solved and added

---
+++ Question12/Q12.js
@@ -0,0 +1,7 @@
+"use strict";
+// Storing names of friends in array
+let names = ["Saleem", "Sadiq", "Rehan", "Kamal"];
+// Printing a personalized message to each friend
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello ${names[i]}, Good Morning!`);
+}
---
+++ Question12/Q12.ts
@@ -0,0 +1,7 @@
+// Storing names of friends in array
+let names: string[] = ["Saleem", "Sadiq", "Rehan", "Kamal"];
+
+// Printing a personalized message to each friend
+for(let i = 0; i < names.length; i++) {
+    console.log(`Hello ${names[i]}, Good Morning!`);
+}
