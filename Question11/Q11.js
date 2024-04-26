d1661172fe08b0e8b6a31c8f77321f2a74f0f267
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-26 14:43:19 +0500

    Question11 solved and added

---
+++ Question11/Q11.js
@@ -0,0 +1,7 @@
+"use strict";
+// Storing names of friends in an array called names
+let names = ["Atif", "Sumair", "Amir", "Adnan"];
+// Printing each name by accessing each element in the list, one at a time
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Question11/Q11.ts
@@ -0,0 +1,9 @@
+// Storing names of friends in an array called names
+
+let names: string[] = ["Atif", "Sumair", "Amir", "Adnan"];
+
+// Printing each name by accessing each element in the list, one at a time
+
+for (let i=0; i<names.length; i++) {
+    console.log(names[i]);
+}
