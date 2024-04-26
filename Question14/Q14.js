4d434344577b44f34f67d961c8f2256a195c1250
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-26 22:06:59 +0500

    Question14 solved and added

---
+++ Question14/Q14.js
@@ -0,0 +1,7 @@
+"use strict";
+// Storing guest names in array
+let guestList = ["Zeeshan", "Rehan", "Khalid"];
+// Printing invitation message to each guest
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]}, \n\nYou are cordially invited to a dinner party tomorow.\n\nRegards, \nIrfan Fayyaz`);
+}
---
+++ Question14/Q14.ts
@@ -0,0 +1,11 @@
+// Storing guest names in array
+
+let guestList: string[] = ["Zeeshan", "Rehan", "Khalid"];
+
+// Printing invitation message to each guest
+
+for (let i = 0; i < guestList.length; i++) {
+  console.log(
+    `Dear Mr. ${guestList[i]}, \n\nYou are cordially invited to a dinner party tomorow.\n\nRegards, \nIrfan Fayyaz`
+  );
+}
