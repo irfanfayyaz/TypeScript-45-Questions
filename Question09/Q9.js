90a7e85c91060212a1e51bef99134ab5437993f6
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-26 10:06:56 +0500

    Question09 solved and added

---
+++ Question09/Q9.js
@@ -0,0 +1,7 @@
+"use strict";
+// Storing favourite number in a variable:
+let favouriteNumber = 13;
+// Creating message that reveals my favourite number:
+let message = (`My favourite number is ${favouriteNumber}`);
+// Printing that message:
+console.log(message);
---
+++ Question09/Q9.ts
@@ -0,0 +1,8 @@
+// Storing favourite number in a variable:
+let favouriteNumber: Number = 13;
+
+// Creating message that reveals my favourite number:
+let message: string = (`My favourite number is ${favouriteNumber}`);
+
+// Printing that message:
+console.log(message);
