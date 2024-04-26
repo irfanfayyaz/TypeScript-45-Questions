4542340bad6cf60e0cd45c736b8b7c8dfbd7ac92
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-26 09:03:57 +0500

    Question06 solved and added

---
+++ Question06/Q6.js
@@ -0,0 +1,6 @@
+"use strict";
+// Stripping Names
+let personName = `\n\t Salman Ali \t\n`;
+console.log(personName);
+let stripped = personName.trim();
+console.log(stripped);
---
+++ Question06/Q6.ts
@@ -0,0 +1,6 @@
+// Stripping Names
+
+let personName: string = `\n\t Salman Ali \t\n`;
+console.log(personName);
+let stripped: string = personName.trim();
+console.log(stripped);
