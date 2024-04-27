f308f0b884226ac78e880a48b98619846bd23c11
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-27 13:33:07 +0500

    Question15 solved and added

---
+++ Question15/Q15.ts
@@ -0,0 +1,18 @@
+// Storing guest names in array
+let guestList: string[] = ["Adnan", "Rehan", "Zeeshan", "Furqan"];
+
+// Printing invitation message for each guest
+for(let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]}, \n\nYou are cordially invited to a dinner party tomorrow.\n\nRegards,\nIrfan Fayyaz`);
+};
+// Replacing the name of guest who can't join with new guest
+let notComing: string = "Adnan";
+let newGuest: string = "Farooq";
+guestList[0] = newGuest;
+
+// Printing the invitation message for new guest list
+for(let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]}, \n\nYou are cordially invited to a dinner party tomorrow.\n\nRegards,\nIrfan Fayyaz\n`);
+};
+// Printing a message about not coming guest
+console.log(`Mr. ${notComing}, is not coming for that dinner party`);
