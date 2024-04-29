a56ccc875f93845345ddb03c9f654bfda3520ceb
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-29 13:04:05 +0500

    Question 20 solved and added in repo

---
+++ Question20/Q20.js
@@ -0,0 +1,11 @@
+"use strict";
+// Storing name of cities in an array
+let citiesList = ["Karachi", "Lahore", "Islamabad", "Faisalabad", "Multan", "Peshawar"];
+// Printing list of stored cities
+console.log("List of famous cities of Pakistan:", citiesList);
+// Storing & printing name of languages in an array
+let pakistaniLanguages = ["Urdu", "Punjabi", "Pushto", "Balochi"];
+console.log(`\nList of languages spoken in Pakistan`);
+for (let i = 0; i < pakistaniLanguages.length; i++) {
+    console.log(`${pakistaniLanguages[i]}`);
+}
---
+++ Question20/Q20.ts
@@ -0,0 +1,12 @@
+// Storing name of cities in an array
+let citiesList: string[] = ["Karachi", "Lahore", "Islamabad", "Faisalabad", "Multan", "Peshawar"];
+
+// Printing list of stored cities
+console.log("List of famous cities of Pakistan:", citiesList);
+
+// Storing & printing name of languages in an array
+let pakistaniLanguages: string[] = ["Urdu", "Punjabi", "Pushto", "Balochi"];
+console.log(`\nList of languages spoken in Pakistan`)
+for(let i = 0; i < pakistaniLanguages.length; i++){
+    console.log(`${pakistaniLanguages[i]}`)
+}
