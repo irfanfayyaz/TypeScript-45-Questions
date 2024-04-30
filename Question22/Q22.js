960f9e55387ddf5e045dd584d03ea15a87ddcdb0
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-30 07:06:34 +0500

    Question 22 solved and added in repo

---
+++ Question22/Q22.js
@@ -0,0 +1,5 @@
+"use strict";
+let listing = ["Saleem", "Shehzad", "Kamran", "Faisal"];
+console.log(listing[4]);
+console.log(listing[3]);
+console.log(listing[0]);
---
+++ Question22/Q22.ts
@@ -0,0 +1,4 @@
+let listing: string[] = ["Saleem", "Shehzad", "Kamran", "Faisal"]
+console.log(listing[4])
+console.log(listing[3])
+console.log(listing[0]);
