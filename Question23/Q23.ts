1689918836c3914f12dcedfd62398bf3eb5a018f
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-01 12:08:06 +0500

    Question 23 solved and added in repo

---
+++ Question23/Q23.ts
@@ -0,0 +1,44 @@
+// Conditional Tests
+let car = "Subaru";
+
+// TRUE TESTS
+// 1) Equality Test
+console.log("Is car == 'Suabru'? I predict True.");
+console.log(car == 'Subaru'); //True
+
+// 2) Strict Equality Test
+console.log("Is car === 'Subaru'? I predict True.");
+console.log(car === 'Subaru'); //True
+
+// 3) Inequality Test (!= not equal to)
+console.log("Is car != 'Toyota'? I predict True.");
+console.log(car != 'Toyota'); //True
+
+// 4) Strict Inequality Test (!== strictly not equal to)
+console.log("Is car !== 'Toyota'? I predict True.");
+console.log(car !== 'Toyota'); //True
+
+// 5) Greater than Test (>)
+console.log("Is car > 'Alto'? I predict True");
+console.log(car > 'Alto'); //True
+
+// FALSE TESTS
+// 1) Equality Test
+console.log("Is car == undefined? I predict False.");
+console.log(car == undefined); //False
+
+// 2) Strict Equality Test
+console.log("Is car === 'Toyota'? I predict False.");
+console.log(car === 'Toyota'); //False
+
+// 3) Inequality Test (!= not equal to)
+console.log("Is car != 'Subaru'? I predict False.");
+console.log(car != 'Subaru'); //False
+
+// 4) Strict Inequality Test (!== strictly not equal to)
+console.log("Is car !== 'Subaru'? I predict False.");
+console.log(car !== 'Subaru'); //False
+
+// 5) Less than Test (<)
+console.log("Is car < 'Alto'? I predict False");
+console.log(car < 'Alto'); //False
