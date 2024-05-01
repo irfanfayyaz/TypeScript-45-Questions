0a8bfcd86fcd8ec4c02fc5f27fff0b1027c64b79
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-01 19:39:34 +0500

    Question 25 solved and added in repo

---
+++ Question25/Q25.ts
@@ -0,0 +1,12 @@
+// if statement test
+let alien_color = "yellow";
+
+console.log("Pass version:");
+if (alien_color === "yellow") {
+    console.log("Congratulations! you just earned 5 points.\n");
+}
+
+console.log("Fail version:\n");
+if(alien_color === "green"){
+    console.log("Congratulations! you just earned 5 points.");
+}
