fd9ec8bd225dbe6b3e2a60e967eb019617d29155
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-01 20:16:32 +0500

    Question 26 solved and added in repo

---
+++ Question26/Q26.js
@@ -0,0 +1,16 @@
+// if and else statement test
+// let alien_color = "green";
+// // Running if block
+// if (alien_color === "green") {
+//     console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+// } else {
+//     console.log("Congratulations! You just earned 10 points.");
+// }
+// Running else block
+var alien_color = "red";
+if (alien_color === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points.");
+}
---
+++ Question26/Q26.ts
@@ -0,0 +1,19 @@
+// if and else statement test
+// let alien_color = "green";
+
+// // Running if block
+
+// if (alien_color === "green") {
+//     console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+// } else {
+//     console.log("Congratulations! You just earned 10 points.");
+// }
+
+// Running else block
+let alien_color = "red";
+
+if (alien_color === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else {
+    console.log("Congratulations! You just earned 10 points.");
+}
