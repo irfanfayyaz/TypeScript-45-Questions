c0d7175c6d54a097cfa7aeef691a8072af57091e
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-08 07:08:56 +0500

    Question 29 solved and added in repo

---
+++ Question29/Q29.js
@@ -0,0 +1,37 @@
+// fruits array:
+var favouriteFruits = ['Mango', 'Banana', 'Apple', 'Grapes', 'Watermelon'];
+// 1st statement
+if ("".concat(favouriteFruits[0]) === 'Mango') {
+    console.log("I like Mango");
+}
+else {
+    console.log("I don't like this fruit");
+}
+// 2nd statement
+if ("".concat(favouriteFruits[1]) === 'Banana') {
+    console.log("I like Banana");
+}
+else {
+    console.log("I don't like this fruit");
+}
+// 3rd statement
+if ("".concat(favouriteFruits[2]) === 'Apple') {
+    console.log("I like Apple");
+}
+else {
+    console.log("I don't like this fruit");
+}
+// 4th statement
+if ("".concat(favouriteFruits[3]) === 'Chikoo') {
+    console.log("I like Chikoo");
+}
+else {
+    console.log("I don't like this fruit");
+}
+// 5th statement
+if ("".concat(favouriteFruits[4]) === "Peach") {
+    console.log("I like Peach");
+}
+else {
+    console.log("I don't like this fruit");
+}
