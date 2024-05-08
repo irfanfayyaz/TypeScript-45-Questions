685f3e3f91adc52390b21d6d2f98d528a23ef5f5
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-08 06:28:04 +0500

    Question 27 solved and added in repo

---
+++ Question27/Q27.js
@@ -0,0 +1,33 @@
+// Alien color is green
+var alien_color = "Green";
+if (alien_color === 'Green') {
+    console.log("Player earned 5 points");
+}
+else if (alien_color === 'Yellow') {
+    console.log("Player earned 10 points");
+}
+else if (alien_color === 'Red') {
+    console.log("Player earned 15 points");
+}
+// Alien color is Yellow
+alien_color = "Yellow";
+if (alien_color === 'Green') {
+    console.log("Player earned 5 points");
+}
+else if (alien_color === 'Yellow') {
+    console.log("Player earned 10 points");
+}
+else if (alien_color === 'Red') {
+    console.log("Player earned 15 points");
+}
+// Alien color is Red
+alien_color = "Red";
+if (alien_color === 'Green') {
+    console.log("Player earned 5 points");
+}
+else if (alien_color === 'Yellow') {
+    console.log("Player earned 10 points");
+}
+else if (alien_color === 'Red') {
+    console.log("Player earned 15 points");
+}
