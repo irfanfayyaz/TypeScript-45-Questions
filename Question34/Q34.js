0b7c7e149053762be039ce68b3bbf269f072a9b2
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-18 14:41:26 +0500

    Question 34 solved and added in repo

---
+++ Question34/Q34.js
@@ -0,0 +1,14 @@
+"use strict";
+// Storing these pizza names in a array,
+let pizzas = ["Fajita", "BBQ Chicken", "Pepperoni"];
+// use a for loop to print the name of each pizza.
+// for(let i = 0; i < pizzas.length; i++){
+//     console.log(pizzas[i])
+// }
+// Modify for loop to print a sentence using name of pizza instead of printing just the name of the pizza.
+// For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+for (let i = 0; i < pizzas.length; i++) {
+    console.log("I like " + pizzas[i] + " pizza.");
+}
+// Adding a line at the end outside the for loop, such as I really love pizza!
+console.log("\nI really love pizza");
