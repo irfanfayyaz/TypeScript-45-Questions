25fa1db45f622d9c64cd1b2c20a5bf76abec4392
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-19 10:27:22 +0500

    Question 38 solved and added in repo

---
+++ Question38/Q38.ts
@@ -0,0 +1,15 @@
+// function called describe_city() that accepts the name of a city and its country.
+// function should print a simple sentence, such as Karachi is in Pakistan.
+// Give parameter for the country a default value. Call your function for three different cities,
+// at least one of which is not in the default country.
+
+let karachi = "Karachi is in Pakistan";
+let lahore = "Lahore is in Pakistan";
+let paris = "Paris is in France";
+
+function describe_city(city:string){
+    console.log(city) 
+}
+// console.log(paris);
+// console.log(karachi);
+console.log(lahore);
