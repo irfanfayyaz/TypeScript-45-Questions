ad09ae2bd8adccbd838fe7e57e5bb86e2c1c41f9
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-21 15:43:17 +0500

    Question 42 solved and added in repo

---
+++ Question42/Q42.ts
@@ -0,0 +1,14 @@
+// Great Magicians: Start with a copy of your previous program.
+// Write a function called make_great() to modifiy array of magicians by adding phrase "the Great" to each magician’s name.
+// Call show_magicians() to see that the list has actually been modified.
+
+let magiciansList:string[] = ["David Blaine", "Criss Angel", "Dynamo", "Lance Burton", "Derren Brown"]
+let greatMagician: string = "The Great";
+
+for (let i=0; i<magiciansList.length; i++){
+    function make_great(){
+        let simpleFunction = `${greatMagician} magician ${magiciansList[i]}`
+        return simpleFunction
+    } let response = make_great()
+    console.log(response)
+}
