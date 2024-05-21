8a915fa4e42136885f078c9e1cf7e9e2dff026e8
irfanfayyaz <irfan.47326@yahoo.com>
2024-05-21 10:24:04 +0500

    Question 41 solved and added in repo

---
+++ Question41/Q41.js
@@ -0,0 +1,19 @@
+"use strict";
+// Make array of magician’s names. Pass the array to a function called show_magicians(),
+// which prints the name of each magician in the array.
+let magiciansList = ["David Blaine", "Criss Angel", "Dynamo", "Lance Burton", "Derren Brown"];
+// function show_magicians(list:string){
+//     return(`${magiciansList}`)
+// }
+// console.log(magiciansList);
+// for (let i=0; i<magiciansList.length; i++){
+//     function show_magicians(list:string){
+//         return(`${magiciansList}`)
+//     } console.log(magiciansList)
+// }
+for (let i = 0; i < 1; i++) {
+    function show_magicians(list) {
+        console.log(list);
+    }
+    console.log(magiciansList);
+}
---
+++ Question41/Q41.ts
@@ -0,0 +1,24 @@
+// Make array of magician’s names. Pass the array to a function called show_magicians(),
+// which prints the name of each magician in the array.
+
+// Answer No. 01:
+let magiciansList:string[] = ["David Blaine", "Criss Angel", "Dynamo", "Lance Burton", "Derren Brown"]
+// function show_magicians(list:string){
+//     return(`${magiciansList}`)
+// }
+// console.log(magiciansList);
+
+// Answer No. 02
+// for (let i=0; i<magiciansList.length; i++){
+//     function show_magicians(list:string){
+//         return(`${magiciansList}`)
+//     } console.log(magiciansList)
+// }
+
+// Answer No. 03
+for (let i = 0; i <1; i++){
+    function show_magicians(list:string){
+        console.log(list)
+    }
+    console.log(magiciansList)
+}
